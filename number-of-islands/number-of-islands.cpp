b4a66d7495823cb111bdf0516aafadcd249310d7
Aditya Singh <44195387+c9addy@users.noreply.github.com>
2021-07-31 23:03:11 +0530

    Time: 16 ms (65.46%), Space: 9.6 MB (47.99%) - LeetHub

--- number-of-islands/number-of-islands.cpp
+++ number-of-islands/number-of-islands.cpp
@@ -1,29 +1,30 @@
-class Solution {
+class Solution {    
 public:
-    vector<int> dx={-1,1,0,0};
-    vector<int> dy={0,0,-1,1};
+    vector<int> dx{-1,1,0,0};
+    vector<int> dy{0,0,-1,1};
     bool issafe(int x,int y,int n,int m){
-        if(x>=n || x<0 || y>=m || y<0) return false;
-        return true;
-    }
-    void bfs(vector<vector<bool>> &vis,vector<vector<char>> &vec,int n,int m,int x,int y){
-        vis[x][y]=true; 
+        if(x<0 || x>=n || y<0 || y>=m)return false;
+            return true;
+        }
+    
+    void bfs(vector<vector<char>> &v, vector<vector<bool>> &vis, int x,int y,int n,int m){
+        vis[x][y]=true;
         for(int i=0;i<4;i++){
-            if(issafe(x+dx[i],y+dy[i],n,m) && vec[x+dx[i]][y+dy[i]]=='1' &&!vis[x+dx[i]][y+dy[i]]){
-                bfs(vis,vec,n,m,x+dx[i],y+dy[i]);
+            int nx=x+dx[i],ny=y+dy[i];
+            if(issafe(nx,ny,n,m) && !vis[nx][ny] && v[nx][ny]=='1'){
+                bfs(v,vis,nx,ny,n,m);
             }
         }
+        return;
     }
-    int numIslands(vector<vector<char>>& vec) {
-        int n=vec.size();
-        int m=vec[0].size();
-        vector<vector<bool> >vis(n,vector<bool>(m,false));
-        int ans=0;
+    int numIslands(vector<vector<char>>& v) {
+        int n=v.size(), m=v[0].size(),ans(0);
+        vector<vector<bool>> vis(n,vector<bool>(m,false));
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                if(vec[i][j]=='1' && !vis[i][j]){
-                    bfs(vis,vec,n,m,i,j);
+                if(v[i][j]=='1' && !vis[i][j]){
                     ans++;
+                    bfs(v,vis,i,j,n,m);
                 }
             }
         }
