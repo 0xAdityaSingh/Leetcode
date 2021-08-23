bad4a7999b3bae025d0bcd47775ad9f5d59770dc
Aditya Singh <44195387+c9addy@users.noreply.github.com>
2021-08-23 18:36:45 +0530

    Time: 0 ms (100.00%), Space: 6 MB (68.11%) - LeetHub

--- next-greater-element-iii/next-greater-element-iii.cpp
+++ next-greater-element-iii/next-greater-element-iii.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int nextGreaterElement(int n) {
     auto digits = to_string(n);
-    next_permutation(begin(digits), end(digits));
+    next_permutation(digits.begin(), digits.end());
     auto res = stoll(digits);
     return (res > INT_MAX || res <= n) ? -1 : res;
     }
