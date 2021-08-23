45da29a5ce1fd2dd9fb465ffd86cd6b2137a763c
Aditya Singh <44195387+c9addy@users.noreply.github.com>
2021-08-23 18:51:03 +0530

    Time: 8 ms (74.67%), Space: 5.9 MB (98.01%) - LeetHub

--- roman-to-integer/roman-to-integer.cpp
+++ roman-to-integer/roman-to-integer.cpp
@@ -4,25 +4,18 @@ public:
     switch(x){
         case 'I':
             return 1;
-            break;
         case 'V':
             return 5;
-            break;
         case 'X':
             return 10;
-            break;
         case 'L':
             return 50;
-            break;
         case 'C':
             return 100;
-            break;
         case 'D':
             return 500;
-            break;
         case 'M':
             return 1000;
-            break;
         default:
             break;
     }
