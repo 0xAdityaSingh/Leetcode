3f12d985f90b57dae6a78b880021ca5e20d141c8
Aditya Singh <44195387+0xAdityaSingh@users.noreply.github.com>
2022-06-13 12:33:55 +0530

    Time: 138 ms (62.00%), Space: 67.7 MB (54.10%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.cpp
+++ 53-maximum-subarray/53-maximum-subarray.cpp
@@ -4,14 +4,9 @@ public:
         int ans(INT_MIN), curr(0);
         for(auto num:nums){
             curr+=num;
-            if(curr < 0) curr=0;
             ans=max(ans,curr);
+            if(curr < 0) curr=0;   
         }
-        int maxnum=INT_MIN;
-        for(auto num:nums){
-            maxnum=max(maxnum,num);
-        }
-        if(maxnum<0) return maxnum;
         return ans;
     }
 };
