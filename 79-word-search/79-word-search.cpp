cbe0a753c7832df2f862a7864ed626b038a12041
Aditya Singh <44195387+0xAdityaSingh@users.noreply.github.com>
2022-06-17 22:53:30 +0530

    Time: 855 ms (23.54%), Space: 8 MB (36.27%) - LeetHub

---
+++ 79-word-search/79-word-search.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool dfs(vector<vector<char>>& board,string word,int i,int j){
+        if(!word.size()) return true;
+        if(i<0 || j<0 || i>=board.size() || j>=board[0].size() || board[i][j]!=word[0]) return false;
+        char c=board[i][j];
+        board[i][j]='*';
+        string s=word.substr(1);
+        bool ans=dfs(board,s,i-1,j) || dfs(board,s,i+1,j) || dfs(board,s,i,j-1) || dfs(board,s,i,j+1);
+        board[i][j]=c;
+        return ans;
+    }
+    bool exist(vector<vector<char>>& board, string word) {
+        for(int i=0;i<board.size();i++)
+            for(int j=0;j<board[0].size();j++)
+                if(dfs(board,word,i,j)) return true;
+        return false;
+    }
+};
