4f2283342cf48f8dc09682b8a900db71b82e3e6d
Aditya Singh <44195387+0xAdityaSingh@users.noreply.github.com>
2022-07-02 12:13:45 +0530

    Added solution - LeetHub

--- Swapping pairs make sum equal - GFG/swapping-pairs-make-sum-equal.cpp
+++ Swapping pairs make sum equal - GFG/swapping-pairs-make-sum-equal.cpp
@@ -6,25 +6,6 @@ using namespace std;
 class Solution{
 
 	public:
-	int getSum(int X[], int n)
-	{
-	    int sum = 0;
-	    for (int i = 0; i < n; i++)
-	        sum += X[i];
-	    return sum;
-	}
-	 
-
-	int getTarget(int A[], int n, int B[], int m){
-
-	    int sum1 = getSum(A, n);
-	    int sum2 = getSum(B, m);
-	 
-
-	    if ((sum1 - sum2) % 2 != 0)
-	        return INT_MIN;
-	    return ((sum1 - sum2) / 2);
-	}
 	bool binary_search(int v[],int s,int e,int val){
 	    if(s>e) return false;
 	    int mid=(s+e)/2;
@@ -41,10 +22,14 @@ class Solution{
         int sum1=0,sum2=0;
         sort(A,A+n);
         sort(B,B+m);
-        int diff=getTarget(A,n,B,m);
-        if(diff==INT_MIN) return -1;
+        for(int i=0;i<n;i++)sum1+=A[i];
+        for(int i=0;i<m;i++)sum2+=B[i];
+        if ((sum1 - sum2) % 2 != 0)
+	        return -1;
+        int diff=(sum1-sum2)/2;
         for(int i=0;i<m;i++){
-            int temp=diff+B[i];
+            int temp=(diff+B[i]);
+            // cout<<diff<<" "<<temp<<endl;
             if(binary_search(A,0,n-1,temp)) return 1;
         }
         return -1;
