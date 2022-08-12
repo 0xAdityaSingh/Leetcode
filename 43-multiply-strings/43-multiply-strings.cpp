7b4853de3fdfd0e7bdc86325c6afa6338259a318
Aditya Singh <44195387+0xAdityaSingh@users.noreply.github.com>
2022-08-12 11:09:37 +0530

    Time: 11 ms (61.05%), Space: 6.8 MB (43.73%) - LeetHub

--- 43-multiply-strings/43-multiply-strings.cpp
+++ 43-multiply-strings/43-multiply-strings.cpp
@@ -1,38 +1,35 @@
 class Solution {
 public:
     string multiply(string num1, string num2) {
-
-        if(num1=="0"|| num2=="0") return "0";
-        
-        int n=num1.size();// len of string num1
-        int m=num2.size();// len of string num2
-        vector<int> num(n+m,0);
-        string ans;
-        
-        int i=n-1;
+        if(num1=="0" || num2=="0") return "0";
+        int n=num1.size();
+        int m=num2.size();
+        vector<int>vec(n+m);
+        int i=m-1;
         int pf=0;
         while(i>=0){
-            int ival=num1[i]-'0';
+            int ival=num2[i]-'0';
             i--;
+            int j=n-1;
             int carry=0;
-            int j=m-1;
-            int k=num.size()-1-pf;
+            int k=vec.size()-1-pf;
             while(j>=0 || carry!=0){
-                int jval=j>=0?num2[j]-'0':0;
+                int jval=j>=0?num1[j]-'0':0;
                 j--;
-                int prod=ival*jval+carry+num[k];
-                num[k]=prod%10;
+                int prod=ival*jval+carry+vec[k];
+                vec[k]=prod%10;
                 carry=prod/10;
                 k--;
             }
             pf++;
         }
+        string ans="";
         bool flag=false;
-        for(auto it: num){
-            if(it==0 and !flag) continue;
+        for(auto it:vec){
+            if(flag==false and it==0) continue;
             else{
-                flag=true;
-                ans+=(it+'0');
+                flag =true;
+                ans+=it+'0';
             }
         }
         return ans;
